0273bd9cb6e0aa326e73ebc1ef87d1485561a97e
Donadei1203 <gu1203.doman@gmail.com>
2025-08-23 00:48:23 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let produtos = [] 
+
+// Conectar e carregar o JSON
+fetch('produtos.json')
+  .then(resposta => resposta.json())
+  .then(corpo => {
+    produtos = corpo
+    mostrarProduto(0) 
+  })
+
+
+function mostrarProduto(indice) {
+  const produto = produtos[indice]
+
+  document.getElementById('imagem').innerHTML = produto.imagem
+  document.getElementById('nome').innerHTML = produto.nome
+  document.getElementById('disco').innerHTML = produto.album
+  document.getElementById('estilo').innerHTML = produto.genero
+  document.getElementById('preco').innerHTML = produto.preco
+}
+
+// Função chamada ao clicar em "Comprar"
+function comprar() {
+  const nomeDisco = document.getElementById('disco').innerText
+  alert(`Você comprou o álbum ${nomeDisco}`)
+}
